9b697a0ddc1d3c8ac8f7c7c10e1a045ad3226e32
Michael T <bravmech@gmail.com>
2020-08-10 12:39:15 +0500

    add test

--- dfs.py
+++ dfs.py
@@ -29,6 +29,25 @@ def test_tim():
     assert explored == {'s', 'a', 'b', 'c', 'd', 'e'}
 
 
+def test_dasgupta():
+    graph = {
+        'a': ['b', 'd'],
+        'b': ['a', 'e', 'f'],
+        'c': ['a', 'f'],
+        'd': ['a', 'g', 'h'],
+        'e': ['b', 'i', 'j'],
+        'f': ['b', 'c'],
+        'g': ['d', 'h'],
+        'h': ['d', 'g'],
+        'i': ['e', 'j'],
+        'j': ['e', 'i'],
+        'k': ['l'],
+        'l': ['k'],
+    }
+    explored = dfs(graph, 'a')
+    assert explored == {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+
+
 if __name__ == '__main__':
     graph = {
         's': ['a', 'b'],
