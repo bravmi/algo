2440362a87d78dec6361c20239b7fdf31230ff32
Michael T <bravmech@gmail.com>
2021-09-28 11:20:07 +0500

    add tests

--- kadane.py
+++ kadane.py
@@ -1,20 +1,28 @@
-def kadane(a: list) -> float:
+def kadane(a: list[float]) -> float:
     """Kadane's algo for maximum sum subarray problem
 
     O(n) time, O(1) space
     """
     best = curr = 0
     for x in a:
-        curr = max(curr + x, 0)
+        curr = max(curr + x, x)
         best = max(best, curr)
     return best
 
 
-def test():
+def tests():
     assert kadane([3, 4, 5]) == 12
     assert kadane([4, -2, -8, 5, -2, 7, 7, 2, -6, 5]) == 19
     assert kadane([-2, -3, -5]) == 0
 
 
+def test_dojo():
+    assert kadane([1, -3, 1, 2, -1]) == 3
+
+
+def test_stackoverflow():
+    assert kadane([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+
+
 if __name__ == '__main__':
-    pass
+    tests()
