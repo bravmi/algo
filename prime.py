ecd1b3b5cef6f946d0f309008a8f41660bef2025
Michael T <bravmech@gmail.com>
2021-09-28 11:34:13 +0500

    add type

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 import collections as co
 
 
-def factors(n: int) -> dict:
+def factors(n: int) -> dict[int, int]:
     res: dict = co.defaultdict(lambda: 0)
     i = 2
     while i * i <= n:
@@ -17,7 +17,7 @@ def factors(n: int) -> dict:
 
 def tests():
     assert factors(13195) == {5: 1, 7: 1, 13: 1, 29: 1}
-    assert factors(600851475143) == {5: 1, 7: 1, 13: 1, 29: 1}
+    assert factors(600851475143) == {71: 1, 839: 1, 1471: 1, 6857: 1}
 
 
 if __name__ == '__main__':
